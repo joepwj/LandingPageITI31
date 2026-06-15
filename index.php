5547ff5a2c755c3731e52c7fc01e7f6f9c366e10
joepwj <oliveirajoe23@gmail.com>
2026-06-15 09:52:42 -0300

    codigo inicial do projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    include_once "modulos/topo.php";
+?>
+
+<?php
+    include_once "modulos/rodape.php";
+?>
+<?php
+    include_once "modulos/menu.php";
+?>
+
+<?php
+    include_once "modulos/lateral.php";
+?>
+<?php
+    include_once "modulos/conteudo.php";
+?>
+
